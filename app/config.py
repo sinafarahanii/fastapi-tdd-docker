4a0f2ea6d11e1871c9d3b93502ec876965849a09
Sina Farahani <sinafarahani@aut.ac.ir>
2023-08-23 16:50:14 +0330

    database + pytest

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import logging
 from functools import lru_cache
 from pydantic_settings import BaseSettings
+from pydantic import AnyUrl
 
 
 log = logging.getLogger("uvicorn")
@@ -9,6 +10,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = bool(0)
+    database_url: AnyUrl = None
 
 
 @lru_cache()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
+import os
+
 from fastapi import FastAPI, Query, Depends
 from .config import get_settings, Settings
-
+from tortoise.contrib.fastapi import register_tortoise
 app = FastAPI(title="TEST")
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
